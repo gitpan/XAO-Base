e12ec2e26876d6ddfb7d9995fe6b064813fa4c0d
Andrew Maltsev <am@ejelta.com>
2005-01-13 23:14:41 +0000

    Import of AMALTSEV/XAO-Base-1.06b from CPAN.
    gitpan-cpan-distribution: XAO-Base
    gitpan-cpan-version:      1.06b
    gitpan-cpan-path:         AMALTSEV/XAO-Base-1.06b.tar.gz
    gitpan-cpan-author:       AMALTSEV
    gitpan-cpan-maturity:     released

--- lib/XAO/Cache.pm
+++ lib/XAO/Cache.pm
@@ -47,7 +47,7 @@ use XAO::Errors qw(XAO::E::Cache);
 use XAO::Objects;
 
 use vars qw($VERSION);
-($VERSION)=(q$Id: Cache.pm,v 1.3 2002/02/20 01:03:28 am Exp $ =~ /(\d+\.\d+)/);
+$VERSION=(0+sprintf('%u.%03u',(q$Id: Cache.pm,v 2.1 2005/01/13 22:34:34 am Exp $ =~ /\s(\d+)\.(\d+)\s/))) || die "Bad VERSION";
 
 ###############################################################################
 
